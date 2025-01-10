9144ec80d2da74b4550622fa083487ba720940d0
hwang105814 <83663987+SeongMun-Hwang@users.noreply.github.com>
2025-01-10 11:49:27 +0900

    손 조작 프로토타입 3

--- Assets/_Scripts/SM_Hwang/HandController.cs
+++ Assets/_Scripts/SM_Hwang/HandController.cs
@@ -12,22 +12,25 @@ public class HandController : MonoBehaviour
     [SerializeField] Rigidbody rightHand;
 
     Vector3 targetVelocity;
-    Vector3 prevVelocity =Vector3.zero;
+    Vector3 prevVelocity = Vector3.zero;
     [SerializeField] float moveDelay = 0.5f; //이동 시작 딜레이
     [SerializeField] float smoothFactor = 0.1f; //관성
-    [SerializeField] float multiflier = 10f;
+    [SerializeField] float multiflier = 10f; //감도 배수
 
     float mouseX;
     float mouseY;
 
+    float mouseNotMovedTime = 0f; //마우스 이동이 없던 시간
+
     [SerializeField] TextMeshProUGUI velocityTmp;
+    [SerializeField] TextMeshProUGUI inputTmp;
     [SerializeField] TextMeshProUGUI multiflierTmp;
 
     private void Start()
     {
         //Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
-        
+
     }
     private void Update()
     {
@@ -36,6 +39,7 @@ public class HandController : MonoBehaviour
             SceneManager.LoadScene("SM_Hwang");
         }
         UpdateMultifly();
+        CheckMouseInput();
     }
     private void FixedUpdate()
     {
@@ -52,9 +56,10 @@ public class HandController : MonoBehaviour
         }
         else
         {
-            leftHand.linearVelocity = Vector3.Lerp(leftHand.linearVelocity, Vector3.zero, smoothFactor);
-            rightHand.linearVelocity = Vector3.Lerp(rightHand.linearVelocity, Vector3.zero, smoothFactor);
+            StopHandsMovement();
         }
+        LimitHandPosition(leftHand);
+        LimitHandPosition(rightHand);
     }
     /* 마우스 이동  속도에 따른 가속을 주는 함수
      * mouseX,mouseY가 대체로 1이하의 값이 나와 적절한 multiflier를 곱한 후 제곱을 통해 속도 증가
@@ -65,8 +70,8 @@ public class HandController : MonoBehaviour
         {
             mouseX = Input.GetAxis("Mouse X");
             mouseY = Input.GetAxis("Mouse Y");
-
-            targetVelocity = new Vector3(mouseX * Mathf.Pow(mouseX * multiflier, 2), mouseY* Mathf.Pow(mouseY * multiflier, 2), 0)  ;
+            inputTmp.text = "x : " + mouseX + " y : " + mouseY;
+            targetVelocity = new Vector3(mouseX * Mathf.Pow(mouseX * multiflier, 2), mouseY * Mathf.Pow(mouseY * multiflier, 2), 0);
         }
     }
     void MoveHandAfterDelay(Rigidbody hand)
@@ -77,25 +82,46 @@ public class HandController : MonoBehaviour
             velocityTmp.text = hand.linearVelocity.ToString();
             // `prevVelocity`를 현재 손의 속도로 갱신
             prevVelocity = Vector3.Lerp(prevVelocity, targetVelocity, smoothFactor);
-
-            // 갱신된 속도를 코루틴으로 적용
-            StartCoroutine(UpdateVelocity(hand, prevVelocity));
-            LimitHandPosition(hand);
         }
-        //else
-        //{
-        //    hand.linearVelocity = Vector3.Lerp(hand.linearVelocity, Vector3.zero, smoothFactor);
-        //}
+        // 갱신된 속도를 코루틴으로 적용
+        StartCoroutine(UpdateVelocity(hand, prevVelocity));
     }
 
     /*MoveDelay 이후에 사용자의 마우스 움직임에 따른 velocity를 갱신한는 함수
-     * 사용자의 마우스 컨트롤에 의한 손 이동이 즉각적이면 너무 쉽고 단조로울 거 같아 작성
+     사용자의 마우스 컨트롤에 의한 손 이동이 즉각적이면 너무 쉽고 단조로울 거 같아 작성
      테스트를 통한 조정 필요*/
     IEnumerator UpdateVelocity(Rigidbody hand, Vector3 velocity)
     {
         yield return new WaitForSeconds(moveDelay);
         hand.linearVelocity = velocity;
     }
+    /*마우스 이동이 있는 시간을 체크하고 일정 시간 동안 없으면 멈추는 함수
+     시간을 체크하는 이유 : 이동이 없을 때 바로 멈추면 잠깐의 순간도 즉각적으로
+    체크해서 기본적인 움직임이 뚝뚝 끊기게 됨*/
+    void CheckMouseInput()
+    {
+        if (mouseX == 0 && mouseY == 0)
+        {
+            mouseNotMovedTime += Time.deltaTime;
+            if (mouseNotMovedTime > 0.5f)
+            {
+                StopHandsMovement();
+            }
+        }
+        else
+        {
+            mouseNotMovedTime = 0;
+        }
+    }
+    /*모든 손 움직임을 멈추는 함수*/
+    void StopHandsMovement()
+    {
+        leftHand.linearVelocity = Vector3.Lerp(leftHand.linearVelocity, Vector3.zero, smoothFactor);
+        rightHand.linearVelocity = Vector3.Lerp(rightHand.linearVelocity, Vector3.zero, smoothFactor);
+    }
+
+    /*손 위치가 화면 밖으로 나가지 않게 하는 함수
+     카메라 경계 부분에서 드드득 거려 개선 필요*/
     void LimitHandPosition(Rigidbody hand)
     {
         Vector3 viewportPos = Camera.main.WorldToViewportPoint(hand.position);
@@ -104,6 +130,7 @@ public class HandController : MonoBehaviour
         Vector3 worldPos = Camera.main.ViewportToWorldPoint(viewportPos);
         hand.position = worldPos;
     }
+    /*감도조절 함수*/
     void UpdateMultifly()
     {
         if (Input.GetKeyDown(KeyCode.A))
