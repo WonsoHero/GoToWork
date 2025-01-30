976341a439895813f5dd50e66b10a465d1b26daa
hwang105814 <83663987+SeongMun-Hwang@users.noreply.github.com>
2025-01-30 15:56:14 +0900

    MissionObj Usb 추가

---
+++ Assets/_Scripts/LaptopMission/UsbMission.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class UsbMission : MissionOBJ
+{
+    [SerializeField] Destructible destructible;
+    private void OnEnable()
+    {
+        //파괴가능 오브젝트가 부서지면(다수일 수 있음) 실패
+        destructible.destruct += OnMissionFailed;
+    }
+
+    private void OnDisable()
+    {
+        destructible.destruct -= OnMissionFailed;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
