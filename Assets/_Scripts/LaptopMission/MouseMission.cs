138b424dadd03b16946c4003406612dd46828655
hwang105814 <83663987+SeongMun-Hwang@users.noreply.github.com>
2025-02-03 11:35:47 +0900

    거실 미션 프로토타입

--- Assets/_Scripts/LaptopMission/MouseMission.cs
+++ Assets/_Scripts/LaptopMission/MouseMission.cs
@@ -6,20 +6,21 @@ using UnityEngine.UI;
 
 public class MouseMission : MissionOBJ
 {
-    [SerializeField] RectTransform mousePointer;
+    [SerializeField] GameObject mousePointer;
     [SerializeField] Canvas windowCanvas;
     [SerializeField] GameObject mouseGameObject;
 
     Camera uiCamera;
     GraphicRaycaster raycaster;
     PointerEventData pointerEventData;
-    List<RaycastResult> raycastResults=new List<RaycastResult>();
+    List<RaycastResult> raycastResults = new List<RaycastResult>();
     Vector3 lastMousePosition;  // 이전 프레임의 마우스 오브젝트 위치
     void Start()
     {
-        uiCamera=Camera.main;
-        raycaster=windowCanvas.GetComponent<GraphicRaycaster>();
+        uiCamera = Camera.main;
+        raycaster = windowCanvas.GetComponent<GraphicRaycaster>();
         pointerEventData = new PointerEventData(EventSystem.current);
+        lastMousePosition=mouseGameObject.transform.position;
     }
     void Update()
     {
@@ -27,7 +28,7 @@ public class MouseMission : MissionOBJ
         {
             Vector3 delta = mouseGameObject.transform.position - lastMousePosition;
             delta.y = 0; // y 값은 무시
-            MovePointer(delta*2);
+            MovePointer(delta * 2);
             lastMousePosition = mouseGameObject.transform.position;
         }
         if (Input.GetMouseButtonDown(0))
@@ -40,7 +41,7 @@ public class MouseMission : MissionOBJ
     {
         Vector3 localDelta = windowCanvas.transform.InverseTransformVector(delta);
 
-        Vector3 newPosition = mousePointer.anchoredPosition + new Vector2(localDelta.x, localDelta.z);
+        Vector3 newPosition = mousePointer.GetComponent<RectTransform>().anchoredPosition + new Vector2(localDelta.x, localDelta.z);
 
         // 캔버스 내에서 위치 제한
         RectTransform canvasRect = windowCanvas.GetComponent<RectTransform>();
@@ -48,13 +49,13 @@ public class MouseMission : MissionOBJ
         newPosition.y = Mathf.Clamp(newPosition.y, -canvasRect.rect.height / 2, canvasRect.rect.height / 2);
 
         // 적용
-        mousePointer.anchoredPosition = newPosition;
+        mousePointer.GetComponent<RectTransform>().anchoredPosition = newPosition;
     }
     /*World position 캔버스의 버튼을 클릭하는 함수*/
     void ClickButtonAtPointer()
     {
         // 1. 포인터의 월드 좌표를 스크린 좌표로 변환
-        Vector2 screenPoint = RectTransformUtility.WorldToScreenPoint(uiCamera, mousePointer.position);
+        Vector2 screenPoint = RectTransformUtility.WorldToScreenPoint(uiCamera, mousePointer.GetComponent<RectTransform>().position);
 
         // 2. PointerEventData에 스크린 좌표 설정
         pointerEventData.position = screenPoint;
@@ -69,10 +70,9 @@ public class MouseMission : MissionOBJ
             Button button = result.gameObject.GetComponent<Button>();
             if (button != null)
             {
-                button.onClick.Invoke();
+                OnMissionSuccess(true);
                 break;
             }
         }
     }
-
 }
